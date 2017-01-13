55e3694e23a0025306a02bd325a00c237045ffaf
xinghang <xinghang@thunder.com.cn>
2017-01-13 15:12:56 +0800

    update

--- jssum.js
+++ jssum.js
@@ -20,9 +20,26 @@ console.log(a.slice(-1));//['d']
 //array map array to array
 //es6
 [1, 2, 3].map(x => x+1)
+var a = state.map(todo => {
+        if (todo.id === updatedTodo.id) {
+          return { ...todo, ...updatedTodo };
+        } else {
+          return todo;
+      })
 //es5
 [1, 2, 3].map((function(x){
 	return x+1
 }).bind(this))
 
-//
+//array filter array to array
+var a = [1, 2, 3, 4].filter(item => item%2===0)
+console.log(a)
+
+//array push with ajax
+$.getJSON('/test' {param1: 'value1'}, function(data){
+	var items = [];
+	$.each(data, function(key, val){
+		items.push('<li id="'+key+'">val</li>')
+	})
+	$('<ul/>', {'class':'ul-class', html:items.join(',')}).appendTo('body')
+})
