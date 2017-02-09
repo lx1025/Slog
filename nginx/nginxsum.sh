e68db7d1301b29b9d19af9c606d48806f4f6820e
xinghang <xinghang@thunder.com.cn>
2017-02-09 10:36:15 +0800

    update

--- nginx/nginxsum.sh
+++ nginx/nginxsum.sh
@@ -13,31 +13,30 @@ include /etc/nginx/sites-enabled/*;
 这一行加载了一个外部的配置文件，sites-enabled文件夹下只有一个default文件，这个外部的配置文件就是默认的nginx的配置文件
 文件内容：
 server {
-	server_name localhost;   #这里是你的域名，即服务器名称，即访问者的所填写url中的host（主机名），两者对应
-	listen 80 default_server;
-	listen [::]:80 default_server ipv6only=on;
+    server_name localhost;   #这里是你的域名，即服务器名称，即访问者的所填写url中的host（主机名），两者对应
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
 
-	location / {
-		proxy_pass http://127.0.0.1:8000 #实现反向代理这里的意思是从80->8000，这个端口运行着工程  8000是非常关键的，涉及到微信的认证
-	}
-	location /article {
-		proxy_pass http://127.0.0.1:8000/article #二级路由
-		proxy_redirect default ;
-
-	}
+    location / {
+        proxy_pass http://127.0.0.1:8000 #实现反向代理这里的意思是从80->8000，这个端口运行着工程  8000是非常关键的，涉及到微信的认证
+    }
+    location /article {
+        proxy_pass http://127.0.0.1:8000/article #二级路由
+        proxy_redirect default ;
+    }
 }
 文件的另外一个末尾行：
 include /etc/nginx/confid.d/*.conf
 到这个目录下添加文件：test.conf
 写入内容：
 server {
-	listen 8088;     #修改端口号
-	server_name  localhost;
+    listen 8088;     #修改端口号
+    server_name  localhost;
 
-	location / {
-		root /usr/share/nginx/html;        #当访问http://localhost:8088时，定位到此根目录，然后按找顺序查找下列文件
-		index index.html index.htm;
-	}
+    location / {
+        root /usr/share/nginx/html;        #当访问http://localhost:8088时，定位到此根目录，然后按找顺序查找下列文件
+        index index.html index.htm;
+    }
 }   
 访问：http://localhost:8088 出现nginx的欢迎页
 
@@ -47,12 +46,12 @@ sudo nginx -t 查看启动日志，如果启动或重启失败，可以由此查
 
 另：nginx的负载均衡
 upstream backend {
-	ip_hash;    
-	server backend1.example.com;
-	server backend2.example.com;
-	server backend3.example.com;
-	server backend4.example.com;
+    ip_hash;    
+    server backend1.example.com;
+    server backend2.example.com;
+    server backend3.example.com;
+    server backend4.example.com;
 }
 location / {
-	proxy_pass http://backend;
+    proxy_pass http://backend;
 }
