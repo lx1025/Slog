3123d19a940557c2b2966fd51c3ad2781e809758
xinghang <xinghang@thunder.com.cn>
2017-03-15 19:41:56 +0800

    update

--- shell/shellsum.sh
+++ shell/shellsum.sh
@@ -42,3 +42,21 @@ scp -P 3026 xinghang@101.254.157.124:/home/xinghang/work/myktv_cms/wx_order.xlsx
 
 #服务器跳板:
 ssh work@106.75.34.22 -p 22
+
+#查看所有相关进程
+ps -ef | grep pyspider | awk '{print $2}' | xargs kill -9
+
+#mac
+1、将光标移动到行首：ctrl + a
+
+2、将光标移动到行尾：ctrl + e
+
+3、清除屏幕：ctrl + l
+
+4、搜索以前使用命令：ctrl + r
+
+5、清除当前行：ctrl + u
+
+6、清除至当前行尾：ctrl + k
+
+7、单词为单位移动：option + 方向键
