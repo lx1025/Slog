658366d44c28016198ed70e132c4ab72f46cfa5a
xinghang <xinghang@thunder.com.cn>
2017-04-28 14:03:45 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,12 +1,1 @@
-function A(a) {
-    this.x = a;
-    var get = function() {
-        return this.x;
-    }
-    this.print = function() {
-        console.log(get());
-    }
-}
 
-a = new A(1);
-a.print();
